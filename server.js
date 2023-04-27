edba8ce189e1b7927942cea70c6e2f8622d0d60a
Amaan ansari <86655270+amaan0810@users.noreply.github.com>
2023-04-27 12:33:03 +0530

    port const added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _=require('lodash');
+const port=process.env.PORT || 3000;
 
 const app = express();
 app.set("view engine", "ejs");
@@ -132,6 +133,6 @@ app.get("/:customlist", function (req, res) {
 
 // mongoose.connection.close()
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
